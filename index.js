6e7458b0fca9e551e2c8b802477c123240c86780
sasha wangui <sasha.iluku@gmail.com>
2023-06-12 01:17:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        newArray.push(message)
+    }
+
+    return newArray
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i)
+        i--;
+    }
+}
